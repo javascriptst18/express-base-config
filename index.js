51ef824f03e462113a4070d923ce9c80f18eaa83
Jesper Orb <jesper.orb@zocom.se>
2018-08-22 12:13:49 +0200

    All routes complete

--- index.js
+++ index.js
@@ -11,31 +11,28 @@ app.use(express.static('public'));
 app.use(express.json()); // So we can handle JSON-data from the user
 app.use(express.urlencoded({ extended: true })); // So we can handle form-data from the user
 
-let todos = [
-  {
-    title: 'Learn Backend',
-    completed: false
-  },
-  {
-    title: 'Learn to learn',
-    completed: true
-  }
-];
-
 const Todo = mongoose.model('Todo', {
-  title: String,
-  completed: Boolean
+  title: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
 });
 
-app.get('/todos', function(request, response) {
+// ReST API
+
+app.get('/todos', function (request, response) {
   Todo.find({})
     .then((documents) => {
       response.json(documents);
-    })
+    });
 });
 
 app.post('/todos', function (request, response) {
-  const newTodo = new Todo({ title: request.body.title, completed: false });
+  const newTodo = new Todo({ title: request.body.title });
   newTodo.save()
     .then(document => {
       response.json(document);
@@ -43,70 +40,26 @@ app.post('/todos', function (request, response) {
 });
 
 app.get('/todos/:id', function (request, response) {
-  /**
-   * :id is saved as 'request.params.id'. so if we visit '/todos/1'
-   * req.params.id will be 1. '/todos/2' will give us req.params.id === 2
-   * it can be a good idea to convert the number to an int. The value is 
-   * a string from the start.
-   */
-  const todoID = parseInt(request.params.id, 10);
-  /**
-   * I am using index as ID, if we know the position we can grab that value
-   * directly.
-   */
-  const selectedItem = todos[todoID];
-  res.send(selectedItem);
+  Todo.findById(request.params.id)
+    .then(document => {
+      response.json(document);
+    })
 });
 
 app.delete('/todos/:id', function (request, response) {
-  //Convert to int from string 
-  const todoID = parseInt(request.params.id, 10);
-  /**
-   * Filter the array of todos. Each iteration knows about the current
-   * object being examined and the index of that object. If the index
-   * is the same as the ID from the URL, do not return it. Filter will
-   * return every item except the one with the ID that we are looking for
-   */
-  const filteredListOfTodos = todos.filter(function (todo, index){
-    return index !== todoID;
-  })
-  /**
-   * Failsafe, clone(spread syntax...) the array and overwrite the previous array
-   * So we are replacing the original array with a whole new array
-   * instead of manipulating the array directly. 
-   */
-  todos = [...filteredListOfTodos];
-  response.send(todos);
+  Todo.findByIdAndRemove(request.params.id)
+    .then(document => {
+      response.json(document);
+    })
 });
 
 app.patch('/todos/:id', function (request, response) {
-  /**
-   * A patch is like a post but will not create a new value only update
-   * an exisiting one. This means that we need to send a body, the same
-   * as with POST. But we also need the ID of the todo to be patched (updated)
-   */
-  const patchedTodo = request.body;
-  const todoID = parseInt(request.params.id, 10);
-  /**
-   * We need to update a specific todo which means we need to 
-   * mutate one of the items, this is a task for map and not filter or reduce.
-   */
-  const patchedListOfTodos = todos.map(function (todo, index){
-    /**
-     * Same as with delete,look for a specific index. If the index
-     * is matched we will return the updated todo that is sent in BODY
-     */
-    if(index === todoID){
-      return patchedTodo;
-    }
-    /**
-     * If the item is not the index we are looking for, return the original todo.todoID
-     * This means that only one of the items in the new array will be changed.
-     */
-    return todo;
+  Todo.findByIdAndUpdate(request.params.id, {
+    title: request.body.title
   })
-  todos = [...patchedListOfTodos];
-  response.send(todos);
+  .then(document => {
+    response.json(document);
+  })
 });
 
 
